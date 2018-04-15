3e5bbb5c04199af664778da5e28905afb41f2a15
Azlaan4 <azlaanzubair4@gmail.com>
2018-04-15 23:05:14 +0500

    Almost Final Push :/

--- js/script.js
+++ js/script.js
@@ -52,35 +52,6 @@ var options = {
   
   });
 
-
-//   hamsburger activation
-
-document.addEventListener('DOMContentLoaded', function () {
-
-  // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-
-    // Add a click event on each of them
-    $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener('click', function () {
-
-        // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
-
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
-      });
-    });
-  }
-
-});
-
 // smooth scroll animation
 $(document).ready(function() {
   var $menu = $(".navbar-start");
@@ -128,3 +99,4 @@ $(document).ready(function() {
     }
   });
 });
+
